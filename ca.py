9bef50c1f34f877934979cbf880c01e100f6edd0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-24 18:41:34 +0100

    Add files via upload

---
+++ ca.py
@@ -0,0 +1,29 @@
+import copy
+print("\033[47;31m\n\ngive me the list file")
+a=input().strip()
+f1=open(a,"r")
+b=f1.read()
+f1.close()
+c=b.split("\n")
+counter=0
+n="""
+match var0:
+"""
+h="""
+    case $1:
+        var1=\"$2\"
+"""
+#j=copy.copy(g)
+#print(j)
+
+for z in range(0,len(c)):
+    m=c[z].split(",")
+    k=copy.copy(h)
+    if len(m)>1:
+        k=k.replace("$1",m[1])
+        k=k.replace("$2",m[0])
+        n=n+"\n"+copy.copy(k)
+a=a.replace("lst","py")
+f1=open(a,"w")
+f1.write(n)
+f1.close()
---
+++ java.py
@@ -0,0 +1,810 @@
+
+match var0:
+
+
+    case 50:
+        var1="aaload"
+
+
+    case 83:
+        var1="aastore"
+
+
+    case 1:
+        var1="aconst_null"
+
+
+    case 25:
+        var1="aload"
+
+
+    case 42:
+        var1="aload_0"
+
+
+    case 43:
+        var1="aload_1"
+
+
+    case 44:
+        var1="aload_2"
+
+
+    case 45:
+        var1="aload_3"
+
+
+    case 189:
+        var1="anewarray"
+
+
+    case 176:
+        var1="areturn"
+
+
+    case 190:
+        var1="arraylength"
+
+
+    case 58:
+        var1="astore"
+
+
+    case 75:
+        var1="astore_0"
+
+
+    case 76:
+        var1="astore_1"
+
+
+    case 77:
+        var1="astore_2"
+
+
+    case 78:
+        var1="astore_3"
+
+
+    case 191:
+        var1="athrow"
+
+
+    case 51:
+        var1="baload"
+
+
+    case 84:
+        var1="bastore"
+
+
+    case 16:
+        var1="bipush"
+
+
+    case 52:
+        var1="caload"
+
+
+    case 85:
+        var1="castore"
+
+
+    case 192:
+        var1="checkcast"
+
+
+    case 144:
+        var1="d2f"
+
+
+    case 142:
+        var1="d2i"
+
+
+    case 143:
+        var1="d2l"
+
+
+    case 99:
+        var1="dadd"
+
+
+    case 49:
+        var1="daload"
+
+
+    case 82:
+        var1="dastore"
+
+
+    case 152:
+        var1="dcmpg"
+
+
+    case 151:
+        var1="dcmpl"
+
+
+    case 14:
+        var1="dconst_0"
+
+
+    case 15:
+        var1="dconst_1"
+
+
+    case 111:
+        var1="ddiv"
+
+
+    case 24:
+        var1="dload"
+
+
+    case 38:
+        var1="dload_0"
+
+
+    case 39:
+        var1="dload_1"
+
+
+    case 40:
+        var1="dload_2"
+
+
+    case 41:
+        var1="dload_3"
+
+
+    case 107:
+        var1="dmul"
+
+
+    case 119:
+        var1="dneg"
+
+
+    case 115:
+        var1="drem"
+
+
+    case 175:
+        var1="dreturn"
+
+
+    case 57:
+        var1="dstore"
+
+
+    case 71:
+        var1="dstore_0"
+
+
+    case 72:
+        var1="dstore_1"
+
+
+    case 73:
+        var1="dstore_2"
+
+
+    case 74:
+        var1="dstore_3"
+
+
+    case 103:
+        var1="dsub"
+
+
+    case 89:
+        var1="dup"
+
+
+    case 90:
+        var1="dup_x1"
+
+
+    case 91:
+        var1="dup_x2"
+
+
+    case 92:
+        var1="dup2"
+
+
+    case 93:
+        var1="dup2_x1"
+
+
+    case 94:
+        var1="dup2_x2"
+
+
+    case 141:
+        var1="f2d"
+
+
+    case 139:
+        var1="f2i"
+
+
+    case 140:
+        var1="f2l"
+
+
+    case 98:
+        var1="fadd"
+
+
+    case 48:
+        var1="faload"
+
+
+    case 81:
+        var1="fastore"
+
+
+    case 150:
+        var1="fcmpg"
+
+
+    case 149:
+        var1="fcmpl"
+
+
+    case 11:
+        var1="fconst_0"
+
+
+    case 12:
+        var1="fconst_1"
+
+
+    case 13:
+        var1="fconst_2"
+
+
+    case 110:
+        var1="fdiv"
+
+
+    case 23:
+        var1="fload"
+
+
+    case 34:
+        var1="fload_0"
+
+
+    case 35:
+        var1="fload_1"
+
+
+    case 36:
+        var1="fload_2"
+
+
+    case 37:
+        var1="fload_3"
+
+
+    case 106:
+        var1="fmul"
+
+
+    case 118:
+        var1="fneg"
+
+
+    case 114:
+        var1="frem"
+
+
+    case 174:
+        var1="freturn"
+
+
+    case 56:
+        var1="fstore"
+
+
+    case 67:
+        var1="fstore_0"
+
+
+    case 68:
+        var1="fstore_1"
+
+
+    case 69:
+        var1="fstore_2"
+
+
+    case 70:
+        var1="fstore_3"
+
+
+    case 102:
+        var1="fsub"
+
+
+    case 180:
+        var1="getfield"
+
+
+    case 178:
+        var1="getstatic"
+
+
+    case 167:
+        var1="goto"
+
+
+    case 200:
+        var1="goto_w"
+
+
+    case 145:
+        var1="i2b"
+
+
+    case 146:
+        var1="i2c"
+
+
+    case 135:
+        var1="i2d"
+
+
+    case 134:
+        var1="i2f"
+
+
+    case 133:
+        var1="i2l"
+
+
+    case 147:
+        var1="i2s"
+
+
+    case 96:
+        var1="iadd"
+
+
+    case 46:
+        var1="iaload"
+
+
+    case 126:
+        var1="iand"
+
+
+    case 79:
+        var1="iastore"
+
+
+    case 2:
+        var1="iconst_m1"
+
+
+    case 3:
+        var1="iconst_0"
+
+
+    case 4:
+        var1="iconst_1"
+
+
+    case 5:
+        var1="iconst_2"
+
+
+    case 6:
+        var1="iconst_3"
+
+
+    case 7:
+        var1="iconst_4"
+
+
+    case 8:
+        var1="iconst_5"
+
+
+    case 108:
+        var1="idiv"
+
+
+    case 165:
+        var1="if_acmpeq"
+
+
+    case 166:
+        var1="if_acmpne"
+
+
+    case 159:
+        var1="if_icmpeq"
+
+
+    case 160:
+        var1="if_icmpne"
+
+
+    case 161:
+        var1="if_icmplt"
+
+
+    case 162:
+        var1="if_icmpge"
+
+
+    case 163:
+        var1="if_icmpgt"
+
+
+    case 164:
+        var1="if_icmple"
+
+
+    case 153:
+        var1="ifeq"
+
+
+    case 154:
+        var1="ifne"
+
+
+    case 155:
+        var1="iflt"
+
+
+    case 156:
+        var1="ifge"
+
+
+    case 157:
+        var1="ifgt"
+
+
+    case 158:
+        var1="ifle"
+
+
+    case 199:
+        var1="ifnonnull"
+
+
+    case 198:
+        var1="ifnull"
+
+
+    case 132:
+        var1="iinc"
+
+
+    case 21:
+        var1="iload"
+
+
+    case 26:
+        var1="iload_0"
+
+
+    case 27:
+        var1="iload_1"
+
+
+    case 28:
+        var1="iload_2"
+
+
+    case 29:
+        var1="iload_3"
+
+
+    case 104:
+        var1="imul"
+
+
+    case 116:
+        var1="ineg"
+
+
+    case 193:
+        var1="instanceof"
+
+
+    case 186:
+        var1="invokedynamic"
+
+
+    case 185:
+        var1="invokeinterface"
+
+
+    case 183:
+        var1="invokespecial"
+
+
+    case 184:
+        var1="invokestatic"
+
+
+    case 182:
+        var1="invokevirtual"
+
+
+    case 128:
+        var1="ior"
+
+
+    case 112:
+        var1="irem"
+
+
+    case 172:
+        var1="ireturn"
+
+
+    case 120:
+        var1="ishl"
+
+
+    case 122:
+        var1="ishr"
+
+
+    case 54:
+        var1="istore"
+
+
+    case 59:
+        var1="istore_0"
+
+
+    case 60:
+        var1="istore_1"
+
+
+    case 61:
+        var1="istore_2"
+
+
+    case 62:
+        var1="istore_3"
+
+
+    case 100:
+        var1="isub"
+
+
+    case 124:
+        var1="iushr"
+
+
+    case 130:
+        var1="ixor"
+
+
+    case 168:
+        var1="jsr"
+
+
+    case 201:
+        var1="jsr_w"
+
+
+    case 138:
+        var1="l2d"
+
+
+    case 137:
+        var1="l2f"
+
+
+    case 136:
+        var1="l2i"
+
+
+    case 97:
+        var1="ladd"
+
+
+    case 47:
+        var1="laload"
+
+
+    case 127:
+        var1="land"
+
+
+    case 80:
+        var1="lastore"
+
+
+    case 148:
+        var1="lcmp"
+
+
+    case 9:
+        var1="lconst_0"
+
+
+    case 10:
+        var1="lconst_1"
+
+
+    case 18:
+        var1="ldc"
+
+
+    case 19:
+        var1="ldc_w"
+
+
+    case 20:
+        var1="ldc2_w"
+
+
+    case 109:
+        var1="ldiv"
+
+
+    case 22:
+        var1="lload"
+
+
+    case 30:
+        var1="lload_0"
+
+
+    case 31:
+        var1="lload_1"
+
+
+    case 32:
+        var1="lload_2"
+
+
+    case 33:
+        var1="lload_3"
+
+
+    case 105:
+        var1="lmul"
+
+
+    case 117:
+        var1="lneg"
+
+
+    case 171:
+        var1="lookupswitch"
+
+
+    case 129:
+        var1="lor"
+
+
+    case 113:
+        var1="lrem"
+
+
+    case 173:
+        var1="lreturn"
+
+
+    case 121:
+        var1="lshl"
+
+
+    case 123:
+        var1="lshr"
+
+
+    case 55:
+        var1="lstore"
+
+
+    case 63:
+        var1="lstore_0"
+
+
+    case 64:
+        var1="lstore_1"
+
+
+    case 65:
+        var1="lstore_2"
+
+
+    case 66:
+        var1="lstore_3"
+
+
+    case 101:
+        var1="lsub"
+
+
+    case 125:
+        var1="lushr"
+
+
+    case 131:
+        var1="lxor"
+
+
+    case 194:
+        var1="monitorenter"
+
+
+    case 195:
+        var1="monitorexit"
+
+
+    case 197:
+        var1="multianewarray"
+
+
+    case 187:
+        var1="new"
+
+
+    case 188:
+        var1="newarray"
+
+
+    case 0:
+        var1="nop"
+
+
+    case 87:
+        var1="pop"
+
+
+    case 88:
+        var1="pop2"
+
+
+    case 181:
+        var1="putfield"
+
+
+    case 179:
+        var1="putstatic"
+
+
+    case 169:
+        var1="ret"
+
+
+    case 177:
+        var1="return"
+
+
+    case 53:
+        var1="saload"
+
+
+    case 86:
+        var1="sastore"
+
+
+    case 17:
+        var1="sipush"
+
+
+    case 95:
+        var1="swap"
+
+
+    case 170:
+        var1="tableswitch"
+
+
+    case 196:
+        var1="wide"
